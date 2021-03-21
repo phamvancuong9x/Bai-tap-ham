35012bcf2c69174a595ebe5cc566f2832ec53a40
phamvancuong9x <80909286+phamvancuong9x@users.noreply.github.com>
2021-03-22 00:03:31 +0700

    Add files via upload

---
+++ Nhaptuoi_namsinh.cpp
@@ -0,0 +1,23 @@
+﻿//5. Viết hàm nhập vào tuổi -> xuất ra năm sinh
+#include<iostream>
+using namespace std;
+void nhaptuoi_namsinh() {
+	int tuoi;
+	int Nam_HT;
+	do {
+		cout << "Nhap so tuoi : ";
+		cin >> tuoi;
+	} while (tuoi < 0);
+	do {
+		cout << "Nhap nam hien tai : ";
+		cin >> Nam_HT;
+	} while (Nam_HT<0);
+	cout << "nam sinh la " << Nam_HT - tuoi << endl;
+}
+
+int main()
+{
+	nhaptuoi_namsinh();
+system("pause");
+ return 0;
+}
