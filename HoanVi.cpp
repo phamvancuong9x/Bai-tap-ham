99c7dd2e80eaf50b2399fd7ccd2c6f9ac5f14060
phamvancuong9x <80909286+phamvancuong9x@users.noreply.github.com>
2021-03-21 23:43:16 +0700

    Add files via upload

---
+++ HoanVi.cpp
@@ -0,0 +1,24 @@
+﻿//4. Viết ct hoán vị 2 số nguyên a,b
+#include<iostream>
+using namespace std;
+void hoanvi(int& a, int& b)
+{
+	int term;
+	term = a;
+	a = b;
+	b = term;
+
+}
+int main()
+{
+	int a, b;
+	cout << "Nhap a = ";
+	cin >> a;
+	cout << "Nhap b =";
+	cin >> b;
+	hoanvi(a, b);
+	cout << "Gia tri sau hoan vi a = " << a << endl;
+	cout << "Gia tri sau hoan vi b = " << b << endl;
+	system("pause");
+	return 0;
+}
